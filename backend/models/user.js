a8d9e72e77116c506020d4ca52c4e871605564c3
Jkhaled-glitch <khaledjb584@gmail.com>
2023-07-07 13:14:43 +0200

    first commit

---
+++ backend/models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const User = mongoose.model('users',{
+    email :{
+        type: String
+    },
+    userName :{
+        type: String
+    },
+    password :{
+        type: String
+    },
+    accountType :{
+        type: String
+    }
+    
+})
+module.exports= User;
